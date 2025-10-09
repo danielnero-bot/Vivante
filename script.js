09b3cc3722cf823104bd3deecd17f41e0b581f29
DanielNero <nero80311@gmail.com>
2025-10-09 15:40:35 +0100

    modified

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function openLocation(address = "13 Timothy Lane Rumuola, Port Harcourt, Rivers 
     const formattedAddress = address.replace(/ /g, '+');
   window.open(`https://www.google.com/maps/search/?api=1&query=${formattedAddress}`, '_blank');
 }
+function showMenu() {
+  const secondNav = document.querySelector('.second-nav');
+  const button = document.querySelector('#menu-btn>i');
+  if (secondNav) {
+    const isHidden = secondNav.style.display === 'none' || getComputedStyle(secondNav).display === 'none';
+    button.classList.toggle('fa-bars', !isHidden);
+    button.classList.toggle('fa-xmark', isHidden);
+    secondNav.style.display = isHidden ? 'block' : 'none';
+  }
+  
+}
